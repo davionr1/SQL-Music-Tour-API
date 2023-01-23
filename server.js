d3b531597c8fe5ef656d4b4b9adc029bd154c213
Davion Roachford <davionr1@icloud.com>
2023-01-23 02:54:39 -0800

    add to controller

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+const bandsController = require('./controllers/bands_controller')
+const eventsController = require('./controllers/events_controller')
+const stagesController = require('./controllers/stages_controller')
+// SEQUELIZE CONNECTION
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/bands', bandsController)
+app.use('/events', eventsController)
+app.use('/stages', stagesController)
+const sequelize = new Sequelize(process.env.PG_URI)
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,8 +24,13 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+// CONTROLLERS 
+
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
+
